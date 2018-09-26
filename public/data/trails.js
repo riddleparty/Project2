c5dd58fe40529c0a56aa00a3ed16c466a2249624
email <phupeter916@gmail.com>
2018-09-25 19:12:04 -0700

    current

---
+++ public/data/trails.js
@@ -0,0 +1,66 @@
+var trails = [
+
+    {
+        id: 1,
+        name:"Bayview Trailhead",
+        location:"Sacramento"
+    },
+
+    {
+        id: 2,
+        name:"Cascade Falls",
+        location:"Tahoe"
+    },
+
+    {
+        id: 3,
+        name:"Eagle Rock",
+        location:"Auburn"
+    },
+
+    {
+        id: 4,
+        name:"Tahoe Rim Trail",
+        location:"Tahoe"
+    },
+
+    {
+        id: 5,
+        name:"Mt. Tallac Trail",
+        location:"South Lake Tahoe"
+    },
+
+    {
+        id: 6,
+        name:"Paige Meadows",
+        location:"Folsom"
+    },
+
+    {
+        id: 7,
+        name:"Yosemite Trail",
+        location:"Yosemite"
+    },
+
+    {
+        id: 8,
+        name:"Red Head Trail",
+        location:"Colorado"
+    },
+
+    {
+        id: 9,
+        name:"Point Reyes Trails",
+        location:"San Francisco"
+    },
+
+    {
+        id: 10,
+        name:"Dark Trails",
+        location:"Marin County"
+    },
+
+
+]
+
+module.exports = trails;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,40 @@
 var db = require("../models");
 
+var path = require("path");
+
+
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.sendFile(path.join(__dirname, "../public/view.html"));
   });
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+
+  app.get("/register", function(req, res) {
+    res.sendFile(path.join(__dirname, ""));
+  });
+
+  app.get("/login", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+  // var title = "space+jam";
+  //   var queryURL = "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=3b48b07d";
+
+  //   $.ajax({
+  //     url: queryURL,
+  //     method: "GET"
+  //   }).then(function(response) {
+  //     console.log(response);
+  //     console.log(response.Runtime);
+  //   });
+
+
+
+
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
