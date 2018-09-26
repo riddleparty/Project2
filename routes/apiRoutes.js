1a0c049d74fe60ee1e6292ae88f5acde58660ce7
email <phupeter916@gmail.com>
2018-09-25 21:16:46 -0700

    current javascript

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,38 @@
 var db = require("../models");
 
 module.exports = function(app) {
+
   // Get all examples
+  /*
   app.get("/api/trails", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
+  */
 
-  // Create a new example
+  // Create a new person
   app.post("/api/hiker", function(req, res) {
-    db.hikers.create(req.body).then(function(dbhikers) {
-      res.json(dbhikers);
-    });
-  });
 
+    console.log(req.body);
+
+    db.Hiker.create(req.body).then(function(newHiker) {
+      db.Hiker.findOne({ where: {
+        trails: req.body.trails
+      }}).then(function(friendRow) {
+        res.json(friendRow);
+      }); // get hiker
+    }); // create hiker
+
+  }); // function
+
+  /*
   // Delete an example by id
   app.delete("/api/hiker/:id", function(req, res) {
     db.hikers.destroy({ where: { id: req.params.id } }).then(function(dbhikers) {
       res.json(dbhikers);
     });
   });
+  */
+
 };
