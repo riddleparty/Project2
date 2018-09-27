71c98098d174d9e3d6e84b3ea6cbab5bf5cf5fb9
email <phupeter916@gmail.com>
2018-09-26 20:10:15 -0700

    mostupdated

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -28,7 +28,7 @@ module.exports = function(app) {
         trails: req.body.trails
       }}).then(function(bestMatch) {
         res.json(bestMatch);
-
+        
           
       }); // get hiker
     }); // create hiker
